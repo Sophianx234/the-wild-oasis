6a2a1de1e6723f1360cc1319cd0b449acb36c58d
Damian X <dx4336969@gmail.com>
2024-08-24 14:13:17 +0000

    add more reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import GlobalStyles from "./styles/GlobalStyles"
 import Button from "./ui/Button"
 import Input from "./ui/Input"
 import Heading from "./ui/Heading"
+import Row from "./ui/Row"
 
 function App() {
  
 
   const StyledApp = styled.div`
-    background-color: orangered ;
     padding: 20px;
   `
 
@@ -16,20 +16,38 @@ function App() {
     <>
     <GlobalStyles/>
     <StyledApp>
+      <Row type='vertical'>
+
+      <Row type='horizontal'>
+
       <Heading as='h1'>
         The Wild Oasis
       </Heading>
+      <div>
+
       <Heading as='h2'>
         Check in and out
       </Heading>
-      <Button onClick={()=>alert('Check in')}>Check in</Button>
-      <Button onClick={()=>alert('Check out')}>Check out</Button>
+
+      <Button variations='primary' size='medium' onClick={()=>alert('Check in')}>Check in</Button>
+      <Button variations='secondary' size='small' onClick={()=>alert('Check out')}>Check out</Button>
+      </div>
+      </Row >
+
+
+      
+      <Row type='vertical'>
       <Heading as='h3'>
           Form
       </Heading>
+      <form>
+
 
       <Input type="number" placeholder="number of guests"></Input>
       <Input type="number" placeholder="number of guests"></Input>
+      </form>
+      </Row>
+      </Row>
       
     </StyledApp>
     </>
