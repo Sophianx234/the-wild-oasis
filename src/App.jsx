f12101b9b04c9f763ac694b522793995968e4b81
Damian X <dx4336969@gmail.com>
2024-08-25 08:30:30 +0000

    setting up pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,36 @@
-import styled from "styled-components"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import Bookings from "./pages/Bookings"
+import Cabins from "./pages/Cabins"
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Login from './pages/Login'
+import Account from './pages/Account'
+import PageNotFound from './pages/PageNotFound'
 import GlobalStyles from "./styles/GlobalStyles"
-import Button from "./ui/Button"
-import Input from "./ui/Input"
-import Heading from "./ui/Heading"
-import Row from "./ui/Row"
 
 function App() {
- 
-
-  const StyledApp = styled.div`
-    padding: 20px;
-  `
-
   return (
     <>
     <GlobalStyles/>
-    <StyledApp>
-      <Row type='vertical'>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/bookings" element={<Bookings/>}/>
+        <Route path="/cains" element={<Cabins/>}/>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/account" element={<Account/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
 
-      <Row type='horizontal'>
-
-      <Heading as='h1'>
-        The Wild Oasis
-      </Heading>
-      <div>
-
-      <Heading as='h2'>
-        Check in and out
-      </Heading>
-
-      <Button variations='primary' size='medium' onClick={()=>alert('Check in')}>Check in</Button>
-      <Button variations='secondary' size='small' onClick={()=>alert('Check out')}>Check out</Button>
-      </div>
-      </Row >
-
-
+        
+      </Routes>
+      </BrowserRouter>
+     </>
       
-      <Row type='vertical'>
-      <Heading as='h3'>
-          Form
-      </Heading>
-      <form>
-
-
-      <Input type="number" placeholder="number of guests"></Input>
-      <Input type="number" placeholder="number of guests"></Input>
-      </form>
-      </Row>
-      </Row>
-      
-    </StyledApp>
-    </>
+    
   )
 }
 
