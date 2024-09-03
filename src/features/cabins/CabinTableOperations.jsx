cfb6f4f52181587026c701fdf77e430e7a3d3c04
Damian X <dx4336969@gmail.com>
2024-09-03 14:16:20 +0000

    touches to html favicon

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,9 +1,15 @@
 import TableOperations from '../../ui/TableOperations'
 import Filter from '../../ui/Filter'
+import SortBy from '../../ui/SortBy'
 function CabinTableOperations() {
     return (
         <TableOperations>
             <Filter/>
+            <SortBy options={[
+                {
+                value: 'name-asc'}
+            ]
+            }/>
             
         </TableOperations>
     )
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,9 @@
+function SortBy() {
+    return (
+        <div>
+            Sort By
+        </div>
+    )
+}
+
+export default SortBy
